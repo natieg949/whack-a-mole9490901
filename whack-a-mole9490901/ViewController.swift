2e8e8376e9e7cac37a0fa349c88a600e3adac5cd
Natalie Goldberg <natalie.goldberg.949@k12.friscoisd.org>
2020-10-08 10:48:36 -0500

    Initial Commit

---
+++ whack-a-mole9490901/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  whack-a-mole9490901
+//
+//  Created by Goldberg, Natalie E on 10/8/20.
+//  Copyright © 2020 Goldberg, Natalie E. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
